74af06621a37cb401570a623b019749ca310b7ca
akshat498 <akshatdhukia23@gmail.com>
2025-01-13 10:39:52 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$('.meet-our-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+
+
+    ]
+});
