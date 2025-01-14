04155fd3b1c3276225c79d5a049c2133d672209f
akshat498 <akshatdhukia23@gmail.com>
2025-01-14 16:07:36 +0530

    solve all mistakes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,40 @@
-$('.meet-our-slider').slick({
+
+$('.animate-slider').slick({
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplaySpeed: 2000,
-    arrows: false,
     dots: true,
+    arrows: false,
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '40px',
     responsive: [
+
         {
-            breakpoint: 1024,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
+                slidesToScroll: 2,
+                centerMode: true,
+                centerPadding: '40px',
             }
         },
         {
-            breakpoint: 768,
+            breakpoint: 786,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '40px',
             }
-        },
-
-
+        }
     ]
 });
+
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-nvabar")
+    document.getElementById("rotats").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
